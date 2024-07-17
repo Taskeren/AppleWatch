89ccc850806b2e1ac2984047791ab1740f95c4c3
k4kgit <145930379+k4kgit@users.noreply.github.com>
2024-07-17 15:20:59 +0100

    push

---
+++ Database/StopwatchDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kanye4King.Database
+{
+    public class Kanye4KingDbContext : DbContext
+    {
+        private const string _connectionString = "Data Source=data.db";
+
+        public DbSet<DbPacket> Packets { get; set; }
+        public DbSet<DbLog> Log { get; set; }
+
+        public Kanye4KingDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options
+            .UseSqlite(_connectionString)
+            .EnableSensitiveDataLogging();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DbPacket>().ToTable("packets");
+            modelBuilder.Entity<DbPacket>().HasIndex(c => c.CreatedAt);
+
+            modelBuilder.Entity<DbLog>().ToTable("log");
+            modelBuilder.Entity<DbLog>().HasIndex(m => m.CreatedAt);
+        }
+    }
+}
